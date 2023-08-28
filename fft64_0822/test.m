fec6b5d6de829b31a58a79f7ce9fff30794f2c02
wyc <wuyancheng@sjtu.edu.cn>
2023-08-29 01:22:56 +0800

    fixed some bugs for the test.m

--- fft64_0822/test.m
+++ fft64_0822/test.m
@@ -16,6 +16,7 @@ inputi = rand(64, 25) - 0.5;
 mixData = fi([inputr, inputi], 1, 16);
 inputr = double(mixData(:, 1:25));
 inputi = double(mixData(:, 26:50));
+input = complex(inputr, inputi);
 
 % generate expected output
 
@@ -26,15 +27,18 @@ actualOuti = zeros(64, 25);
 
 parfor k = 1:25
     [actualOutr(:, k), actualOuti(:, k)] = myfft64(inputr(:, k), inputi(:, k), fft8_lutr, fft8_luti, twiddle_lutr, twiddle_luti, N, w, factor_fft8, factor_twiddle);
-    standardOut(:, k) = fft(inputr(:, k), inputi(:, k));
+    standardOut(:, k) = fft(input(:, k));
+    disp(k);
 end
 
-actualOutr = double(fi(actualOutr, 1, 16));
-actualOuti = double(fi(actualOuti, 1, 16));
+mixedActualOut = fi([actualOutr, actualOuti], 1, 16);
+actualOutr = double(mixedActualOut(:, 1:25));
+actualOuti = double(mixedActualOut(:, 26:50));
 actualOut = complex(actualOutr, actualOuti);
 
-expectedOutr = double(fi(real(standardOut), 1, 16));
-expectedOuti = double(fi(imag(standardOut), 1, 16));
+mixedExpectedOut = fi([real(standardOut), imag(standardOut)], 1, 16);
+expectedOutr = double(mixedExpectedOut(:, 1:25));
+expectedOuti = double(mixedExpectedOut(:, 26:50));
 expectedOut = complex(expectedOutr, expectedOuti);
 
 % compare
